840a461d20dbc8daef42391377a04e01f2eb2147
Pradipta Bora <190050089@iitb.ac.in>
2023-03-14 01:28:38 +0100

    fix compilation

--- src/util.rs
+++ src/util.rs
@@ -6,3 +6,4 @@ pub const OUTGOING_MESSAGE_PERIOD: Duration = Duration::from_millis(1);
 
 pub const WAIT_LEADER_TIMEOUT: Duration = Duration::from_millis(500);
 pub const WAIT_DECIDED_TIMEOUT: Duration = Duration::from_millis(50);
+
